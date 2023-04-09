554404b571a9207b3550a348b21a285b79246aaa
Kaan Uzman <55639112+akaanuzman@users.noreply.github.com>
2023-04-10 01:42:36 +0300

    additional activity and layout exercises

--- app/src/main/java/com/archeva/java101/MainActivity.java
+++ app/src/main/java/com/archeva/java101/MainActivity.java
@@ -3,12 +3,48 @@ package com.archeva.java101;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // log variable
+    // private static final String TAG = "MainActivity";
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private EditText txtFirstNumber, txtSecondNumber;
+    private Button btnAddition;
+    private TextView lblResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initComponents();
+        registerEventHandlers();
     }
+    // Button onTap method.
+    private void registerEventHandlers() {
+        btnAddition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Integer firstNumber = Integer.parseInt(txtFirstNumber.getText().toString());
+                Integer secondNumber = Integer.parseInt(txtSecondNumber.getText().toString());
+                Integer result = firstNumber + secondNumber;
+                lblResult.setText("Result: "+result);
+                Log.i(TAG, "Additional Result: "+result);
+            }
+        });
+    }
+
+    private void initComponents() {
+        txtFirstNumber = findViewById(R.id.txtFirstNumber);
+        txtSecondNumber = findViewById(R.id.txtSecondNumber);
+        btnAddition = findViewById(R.id.btnAddition);
+        lblResult = findViewById(R.id.lblResult);
+    }
+
 }
